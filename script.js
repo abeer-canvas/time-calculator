3f045e8293fd8b59308d7fdcbc30f7d55583595b
Abeer <abeer.technology@gmail.com>
2021-02-13 17:52:17 +0600

    bug fixed

--- script.js
+++ script.js
@@ -40,35 +40,52 @@ const minuteMinus = function(timeArr){
 
   // Changes below
   // For first element
-  const hour1 = Number(timeArr[0].match(hourRegex).join(''));
+  const getHour1 = Number(timeArr[0].match(hourRegex).join(''));
+  
   const minute1 = Number(timeArr[0].match(minuteRegex).join(''));
   const ampm1 = timeArr[0].match(ampmRegex).join('');
-  const firstMins = getTotalMin1(hour1, minute1, ampm1);
+  const hour1 = get24FormatHours(getHour1, ampm1);
+  const firstMins = getTotalMin1(hour1, minute1);
 
   // For second element
-  const hour2 = Number(timeArr[1].match(hourRegex).join(''));
+  const getHour2 = Number(timeArr[1].match(hourRegex).join(''));
+
   const minute2 = Number(timeArr[1].match(minuteRegex).join(''));
   const ampm2 = timeArr[1].match(ampmRegex).join('');
+  const hour2 = get24FormatHours(getHour2, ampm2);
   const secondMins = getTotalMin2(hour2, minute2, ampm1, ampm2);
 
   return Number(secondMins - firstMins);
 };
 
-const getTotalMin1 = function(hour, minute, ampm){
-  // 12am will be 00:00
-  if(hour === 12 && ampm === 'am') hour = 0;
+/**
+///////First = 24 hours formatting////////
+h = 12pm => h = 12
+h > 12pm => h = h+12//done
+h = 12am => h = 0//done
+h > 12am => h = h//
 
-  if(hour !== 12 && ampm === 'pm') hour += 12;
+////////Exception///////
+XXpm -> XXam => am+24
+ */
 
+const get24FormatHours = function(hh, apm){
+  if(hh < 12 && apm === 'pm'){
+    return hh+12;
+  } else if(hh === 12 && apm === 'am'){
+    return 0;
+  } else{
+    return hh;
+  }
+}
+
+const getTotalMin1 = function(hour, minute){
   return (hour*60)+minute;
 }
 
 const getTotalMin2 = function(hour, minute, ampm1, ampm2){
-  if(hour !== 12 && ampm2 === 'pm') {
-    hour += 12;
-  } else if (ampm1 === 'pm' && ampm2 === 'am'){
+  if(ampm1 === 'pm' && ampm2 === 'am') {
     hour += 24;
   }
-
   return (hour*60)+minute;
 }
